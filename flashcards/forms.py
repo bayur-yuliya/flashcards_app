a7a5a3403236ca74f2361f4ad9d8140b8f6c56cf
Baiur Yuliia <juliy14497@gmail.com>
2025-11-26 18:56:43 +0200

    feat: add card counter; style: change css, change display cards with much information

--- flashcards/forms.py
+++ flashcards/forms.py
@@ -4,31 +4,64 @@ from .models import Flashcard, Category
 
 
 class FlashcardForm(forms.ModelForm):
-    first_side = forms.CharField(widget=forms.Textarea(attrs={"maxlength": 250}))
-    second_side = forms.CharField(widget=forms.Textarea(attrs={"maxlength": 250}))
-    category = forms.ModelChoiceField(
-        queryset=Category.objects.all(),
-        initial=0
+    first_side = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "maxlength": 250,
+                "class": "card_textarea",
+                "placeholder": "Первая сторона карточки",
+            }
         )
+    )
+    second_side = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "maxlength": 250,
+                "class": "card_textarea",
+                "placeholder": "Вторая сторона карточки",
+            }
+        )
+    )
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), initial=0)
 
     class Meta:
         model = Flashcard
-        fields = ('category', 'first_side', 'second_side',)
+        fields = (
+            "category",
+            "first_side",
+            "second_side",
+        )
+
+    def clean_first_side(self):
+        first_side = self.cleaned_data['first_side']
+        lines = first_side.split(' \n')
+        return lines
+
+    def clean_second_side(self):
+        second_side = self.cleaned_data['second_side']
+        lines = second_side.split(' \n')
+        return lines
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        instance.first_side = self.clean_first_side()
+        instance.second_side = self.clean_second_side()
+
+        if commit:
+            instance.save()
+        return instance
 
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(
-        max_length=100,
-        widget=forms.TextInput(attrs={'class': 'category-input'})
+        max_length=100, widget=forms.TextInput(attrs={"class": "category-input"})
     )
 
     class Meta:
         model = Category
-        fields = ('name',)
+        fields = ("name",)
 
 
 class CategoryFindForm(forms.Form):
-    model_choice = forms.ModelChoiceField(
-        queryset=Category.objects.all(),
-        initial=0
-    )
+    model_choice = forms.ModelChoiceField(queryset=Category.objects.all(), initial=0)
--- flashcards/views.py
+++ flashcards/views.py
@@ -8,130 +8,170 @@ from .models import Flashcard, Category
 
 
 def create_flashcard(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         form = FlashcardForm()
-        return render(request, 'flashcards/create_flashcard.html', {'title': 'Create card', 'form': form})
+        return render(
+            request,
+            "flashcards/create_and_update_flashcard.html",
+            {"title": "Create card", "form": form},
+        )
     form = FlashcardForm(request.POST)
     if form.is_valid():
         form.save()
-    return redirect(reverse('categories_list'))
+    return redirect(reverse("categories_list"))
 
 
 def update_flashcard(request, flashcard_id):
     current_flashcard = get_object_or_404(Flashcard, id=flashcard_id)
-    if request.method == 'GET':
+    if request.method == "GET":
         form = FlashcardForm(instance=current_flashcard)
-        return render(request, 'flashcards/update_flashcard.html', {'title': 'Update card', 'form': form})
+        return render(
+            request,
+            "flashcards/create_and_update_flashcard.html",
+            {"title": "Update card", "form": form},
+        )
     form = FlashcardForm(request.POST, instance=current_flashcard)
     if form.is_valid():
         form.save()
-    return redirect(reverse('categories_list'))
+    return redirect(reverse("categories_list"))
 
 
 def flashcards_list(request):
     cards = Flashcard.objects.all()
     form = CategoryFindForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CategoryFindForm(request.POST)
         if form.is_valid():
-            category = form.cleaned_data['model_choice']
+            category = form.cleaned_data["model_choice"]
             cards = Flashcard.objects.filter(category=category)
-    return render(request, 'flashcards/flashcards_list.html', {
-        'title': 'Flashcards',
-        'cards': cards,
-        'form': form,
-    })
+    return render(
+        request,
+        "flashcards/flashcards_list.html",
+        {
+            "title": "Flashcards",
+            "cards": cards,
+            "form": form,
+        },
+    )
 
 
 def delete_flashcard(request, flashcard_id):
     card_deleted = Flashcard.objects.get(id=flashcard_id)
     card_deleted.delete()
-    return redirect(reverse('flashcards_list'))
+    return redirect(reverse("flashcards_list"))
 
 
 def create_category(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         form = CategoryForm()
-        return render(request, 'flashcards/create_category.html', {'title': 'Create category', 'form': form})
+        return render(
+            request,
+            "flashcards/create_and_update_category.html",
+            {"title": "Create category", "form": form},
+        )
     form = CategoryForm(request.POST)
     if form.is_valid():
         form.save()
-    return redirect(reverse('categories_list'))
+    return redirect(reverse("categories_list"))
 
 
 def update_category(request, category_id):
     current_category = get_object_or_404(Category, id=category_id)
-    if request.method == 'GET':
+    if request.method == "GET":
         form = CategoryForm(instance=current_category)
-        return render(request, 'flashcards/update_category.html', {'title': 'Update category', 'form': form})
+        return render(
+            request,
+            "flashcards/create_and_update_category.html",
+            {"title": "Update category", "form": form},
+        )
     form = CategoryForm(request.POST, instance=current_category)
     if form.is_valid():
         form.save()
-    return redirect(reverse('categories_list'))
+    return redirect(reverse("categories_list"))
 
 
 def categories_list(request):
     categories = Category.objects.all()
 
-    if 'flashcards' in request.session:
-        del request.session['flashcards']
+    if "flashcards" in request.session:
+        del request.session["flashcards"]
 
-    return render(request, 'flashcards/categories_list.html', {
-        'title': 'Categories',
-        'categories': categories,
-    })
+    return render(
+        request,
+        "flashcards/categories_list.html",
+        {
+            "title": "Categories",
+            "categories": categories,
+        },
+    )
 
 
 def delete_category(request, category_id):
     category_deleted = Category.objects.get(id=category_id)
     category_deleted.delete()
     categories = Category.objects.all()
-    return render(request, 'flashcards/categories_list.html', {
-        'title': 'Categories',
-        'categories': categories,
-    })
+    return render(
+        request,
+        "flashcards/categories_list.html",
+        {
+            "title": "Categories",
+            "categories": categories,
+        },
+    )
 
 
 def learning_flashcards(request, category_id):
     category = get_object_or_404(Category, id=category_id)
+    count_cards_in_category = Flashcard.objects.filter(category=category).count()
 
-    if 'flashcards' not in request.session:
-        flashcards = list(Flashcard.objects.filter(category=category).values_list('id', 'first_side', 'second_side'))
+    if "flashcards" not in request.session:
+        flashcards = list(
+            Flashcard.objects.filter(category=category).values_list(
+                "id", "first_side", "second_side"
+            )
+        )
         if not flashcards:
-            return redirect(reverse('create_flashcard'))
-        request.session['flashcards'] = flashcards
+            return redirect(reverse("create_flashcard"))
+        request.session["flashcards"] = flashcards
     else:
-        flashcards = request.session['flashcards']
+        flashcards = request.session["flashcards"]
 
-    last_card_id = request.session.get('last_card_id')
+    last_card_id = request.session.get("last_card_id")
 
-    if request.method == 'POST' and 'learn' in request.POST and last_card_id:
+    if request.method == "POST" and "learn" in request.POST and last_card_id:
         flashcards = [card for card in flashcards if card[0] != last_card_id]
-        request.session['flashcards'] = flashcards
+        request.session["flashcards"] = flashcards
         if not flashcards:
-            return redirect(reverse('categories_list'))
-    elif 'wrong' in request.POST:
+            return redirect(reverse("categories_list"))
+    elif "wrong" in request.POST:
         pass
-    elif 'complete' in request.POST:
+    elif "complete" in request.POST:
         request.session.clear()
-        return redirect(reverse('categories_list'))
+        return redirect(reverse("categories_list"))
 
     card = random.choice(flashcards)
     while card[0] == last_card_id:
         card = random.choice(flashcards)
-    request.session['last_card_id'] = card[0]
+    request.session["last_card_id"] = card[0]
 
-    last = len(flashcards) == 1
-
-    if request.GET.get('reverse'):
+    current_learn_cards = len(flashcards)
+    last = current_learn_cards == 1
+    current_learn_cards = count_cards_in_category - current_learn_cards + 1
+    if request.GET.get("reverse"):
         card1, card2 = card[2], card[1]
     else:
         card1, card2 = card[1], card[2]
 
-    return render(request, 'flashcards/learning_flashcards.html', {
-        'title': 'Learning cards',
-        'card1': card1,
-        'card2': card2,
-        'category_id': category_id,
-        'last': last,
-    })
+    return render(
+        request,
+        "flashcards/learning_flashcards.html",
+        {
+            "title": "Learning cards",
+            "card1": card1,
+            "card2": card2,
+            "category_id": category_id,
+            "last": last,
+            "current_learn_cards": current_learn_cards,
+            "count_cards_in_category": count_cards_in_category,
+        },
+    )
